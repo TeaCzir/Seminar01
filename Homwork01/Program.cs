99dd441c05ee7b05e83dbc62eba56ad9b156cfa7
TeaCzir <128166070+TeaCzir@users.noreply.github.com>
2023-03-30 03:10:50 +0300

    Число в квадрате

---
+++ Homwork01/Program.cs
@@ -0,0 +1,6 @@
+﻿//Число в квадрате.
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sguared = number * number;
+Console.WriteLine($"Квадтрат числа:{number} равен:{sguared}");
