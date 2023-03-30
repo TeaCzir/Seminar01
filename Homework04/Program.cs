1643ae23b58c3dbcb41dd546c7b87a4c7b310ebc
TeaCzir <128166070+TeaCzir@users.noreply.github.com>
2023-03-30 13:19:49 +0300

    на вход трехзначное, а на выход последнее.

---
+++ Homework04/Program.cs
@@ -0,0 +1,13 @@
+﻿//Трехзначное на вход, на выход последнее.
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+while(number<100||number>999)
+{
+ Console.WriteLine("Введено неверное значение:");
+ Console.Write("Введите трехзначное число: ");
+ number = int.Parse(Console.ReadLine());
+}
+int y = number%10;
+Console.Write("Последняя цифра: ..");
+Console.Write($"{y}");
