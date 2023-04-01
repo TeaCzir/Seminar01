e0501d0e8a95b42d1432830e29ab658203c2a6ca
TeaCzir <128166070+TeaCzir@users.noreply.github.com>
2023-04-01 04:49:26 +0300

    из двух чисел на вход опредильть максимальное.

---
+++ Homework05/Program.cs
@@ -0,0 +1,18 @@
+﻿//Из двух на вход, какое максимальное.
+Console.Clear();
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if(number1 > number2)
+{
+   Console.Write("первое число максимальное.");
+}
+if(number1 < number2)
+{
+   Console.Write("второе число максимальное.");
+}
+if(number1 == number2)
+{
+   Console.Write("Оба числа одинаковые. ");
+}
